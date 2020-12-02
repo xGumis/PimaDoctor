0d8baca06dac7a043508cff1ac8511e34fcf92f0
Mateusz Siedlarz <smateusz96@gmail.com>
2020-12-02 11:44:02 +0100

    added all models and connection to database

---
+++ PimaDoctor/Models/DbCinema.cs
@@ -0,0 +1,14 @@
+﻿using LinqToDB;
+
+namespace PimaDoctor.Models
+{
+    /*
+     * Model that allows connecting to DB and operating on it
+     */
+    public class DbCinema : LinqToDB.Data.DataConnection
+    {
+        public DbCinema() : base("PrimaDoctor") { }
+        public ITable<Role> Roles => GetTable<Role>();
+        public ITable<User> Users => GetTable<User>();
+    }
+}
---
+++ PimaDoctor/Utilities/PasswordCipher.cs
@@ -0,0 +1,27 @@
+﻿﻿using System.Text;
+
+ namespace PimaDoctor.Utilities
+{
+    class PasswordCipher
+    {
+        // standard ROT47 converter
+        private static char ConvertChar(char c)
+        {
+            return c > 32 && c < 127
+                ? (char)(33 + ((c + 14) % 94))
+                : c;
+        }
+
+        public static string ConvertPassword(string password)
+        {
+            StringBuilder encryptedPassword = new StringBuilder();
+
+            for (int i = 0; i < password.Length; i++)
+            {
+                encryptedPassword.Append(ConvertChar(password[i]));
+            }
+
+            return encryptedPassword.ToString();
+        }
+    }
+}
