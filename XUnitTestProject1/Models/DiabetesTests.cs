1ab5fa26196ebc50c893ffb41b52d587659fe378
Mateusz Siedlarz <smateusz96@gmail.com>
2020-12-06 18:15:01 +0100

    added RoleValidatorTests.cs and DiabetesTests.cs

---
+++ XUnitTestProject1/Models/DiabetesTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using PimaDoctor.Models;
+using Xunit;
+
+namespace XUnitTestProject1.Models
+{
+    public class DiabetesTests
+    {
+        [Fact]
+        public void CreateDiabetesTest()
+        {
+            var diabetes = new Diabetes
+            {
+                Age = 54,
+                Glucose = 125,
+                Insulin = 0,
+                Outcome = 1,
+                Pregnancies = 8,
+                BloodPressure = 96,
+                SkinThickness = 0,
+                BMI = 0,
+                DiabetesPedigreeFunction = 0.232
+            };
+
+            diabetes.Should().BeOfType<Diabetes>();
+        }
+    }
+}
---
+++ XUnitTestProject1/Validators/RoleValidatorTests.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using PimaDoctor.Models;
+using PimaDoctor.Validators;
+using Xunit;
+
+namespace XUnitTestProject1.Validators
+{
+    public class RoleValidatorTests
+    {
+        [Fact]
+        public void ListTest()
+        {
+            var roles = RoleValidator.GetAllRoles();
+            roles.Should().BeOfType<List<Role>>();
+        }
+
+        [Fact]
+        public void GetRoleByIdTest()
+        {
+            RoleValidator.RoleAddValidation("testRole");
+            var roleId = RoleValidator.GetRoleByName("testRole").Id;
+            var role = RoleValidator.GetRoleById(roleId);
+            role.Should().BeOfType<Role>();
+            RoleValidator.RoleDeleteValidation(role.Id);
+        }
+
+        [Fact]
+        public void GetRoleByNameTest()
+        {
+            RoleValidator.RoleAddValidation("testRole");
+            var role = RoleValidator.GetRoleByName("testRole");
+            role.Should().BeOfType<Role>();
+            RoleValidator.RoleDeleteValidation(role.Id);
+        }
+
+        [Fact]
+        public void RoleAddValidationTest()
+        {
+            var success = RoleValidator.RoleAddValidation("testRole");
+            success.Should().BeTrue();
+            var role = RoleValidator.GetRoleByName("testRole");
+            RoleValidator.RoleDeleteValidation(role.Id);
+        }
+
+        [Fact]
+        public void RoleUpdateValidationTest()
+        {
+            RoleValidator.RoleAddValidation("testRole");
+            var role = RoleValidator.GetRoleByName("testRole");
+            var success = RoleValidator.RoleUpdateValidation(role.Id, "updatedRole");
+            success.Should().BeTrue();
+            RoleValidator.RoleDeleteValidation(role.Id);
+        }
+
+        [Fact]
+        public void RoleDeleteValidationTest()
+        {
+            RoleValidator.RoleAddValidation("testRole");
+            var role = RoleValidator.GetRoleByName("testRole");
+            var success = RoleValidator.RoleDeleteValidation(role.Id);
+            success.Should().BeTrue();
+        }
+    }
+}
